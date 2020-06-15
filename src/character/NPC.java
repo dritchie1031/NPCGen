a8a7d1c9f3219c84690cdc476d70c4edd91c6ecc
Daniel Ritchie <dritchie1031@gmail.com>
2020-06-14 22:28:06 -0700

    Updated NPC

--- src/character/NPC.java
+++ src/character/NPC.java
@@ -44,7 +44,7 @@ public class NPC {
 		Weight = Generator.generateNum(race.getWeight());
 
 //		if(isCombatant) {
-			Level = Generator.generateNum(new int[] {1,1,1,1,2,2,2,3,3,4,5});
+			Level = Generator.generateNum(new int[] {1,1,1,1,2,2,2,3,3});
 			if(isClassed) {
 				cla = new classtype.Class(Level, ClassName.getClassName(Generator.generateNum(new int[] {0,1,2,3,4,5,6,7,8,9,10,11})));
 			}
